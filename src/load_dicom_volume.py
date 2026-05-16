66beedf61e77b94da2dbda167bf2157882df8437
MrsPomodoro <baska.klimekova@gmail.com>
2026-05-16 15:43:43 +0200

    refactor load_dicom_volume.py and separate execution logic from helper functions

--- src/load_dicom_volume.py
+++ src/load_dicom_volume.py
@@ -10,7 +10,6 @@ import numpy as np
 import pydicom
 import glob
 
-filepath = '../data/Lobus_DICOMs_mixed_up/*.dcm'
 
 # load the DICOM files into list datasets - refactor into helper function
 def load_dicom_files(filepath):
@@ -22,72 +21,15 @@ def load_dicom_files(filepath):
     print(len(datasets), ' slices loaded...')
     return datasets
 
-datasets = load_dicom_files(filepath)
-####     My implementation         ####
-
-# TODO: prepare a 3D numpy array as array of zeros to store the volume intensities,
-#  use the same datatype as it is used in the input dicom files, volume dimensions
-#  should be the slice shape in x-,y- direction and number of slices in z-direction
-#  so that every input slice can go into its correct position
 
 
-## 1. I explored the loaded DICOM datasets before processing
-#  Goal:
-# - understand the basic structure of the datasets
-# - explore important DICOM metadata
-# - identify how the slice order is stored
-# - find the DICOM tag for Instance Number
-##
-print(datasets)           # I found tag for Instance Number (0020,0013)
-
-for dataset in datasets:
-    slice_index = dataset[0x0020, 0x0013].value
-    print(slice_index)    # check the order of the slice via loop of InstanceNumbers
-
-## 2. I explored the shape and datatype of existing slides to be able to create 3D array
-#   Goal:
-#   - get the shape (rows and columns) and datatype of one DICOM slice
-#   - this is needed to create a new 3D volume with the same properties
-
-# Check first DICOM slice
-first_dataset = datasets[0]               ##   - I used .shape and .dtype  numpy functions as in example03
-first_slice = first_dataset.pixel_array   ##   - I used .pixel_array  numpy functions as in example04
-
-# checking logs
-print("First slice shape:", first_slice.shape)
-print("First slice datatype:", first_slice.dtype)
-print("Number of slices:", len(datasets))
-
-# Define rows, columns and number of slices
-rows = first_slice.shape[0]
-columns = first_slice.shape[1]
-number_of_slices = len(datasets)
-datatype = first_slice.dtype
-
-# checking logs
-print("Rows:", rows)
-print("Columns:", columns)
-print("Number of slices:", number_of_slices)
-print("Datatype:", datatype)
-
-
-# 3. I prepared the  function that creates an empty 3D numpy array for the DICOM volume.
+#  I prepared the  function that creates an empty 3D numpy array for the DICOM volume.
 #    Goal:
 #    -  set up volume dimensions: slice shape in x-,y- direction and number of slices in z-direction
 
 def prepare_empty_volume(rows, columns, number_of_slices, datatype):
     volume = np.zeros((rows, columns, number_of_slices), dtype=datatype)
     return volume
-
-#calling that function
-volume3D = prepare_empty_volume(rows, columns, number_of_slices, datatype)
-
-# checking logs
-print("Volume shape:", volume3D.shape)
-print("Volume datatype:", volume3D.dtype)
-print("Volume min intensity:", volume3D.min())
-print("Volume max intensity:", volume3D.max())
-
 
 
 #TODO 2: the list of individual slice images is mixed-up, therefore loop over all datasets:
@@ -97,7 +39,7 @@ print("Volume max intensity:", volume3D.max())
 # pydicom.dataelem.DataElement, use the ‘.value’ attribute to get just the value)
 # store the individual slice in the correct position in the volume
 
-##4. Fill the 3D volume with sorted DICOM slices
+## Fill the 3D volume with sorted DICOM slices
 # Goal:
 # - get correct slice index from DICOM metadata
 # - copy every slice into correct z-position in the 3D volume
@@ -110,6 +52,3 @@ def sort_and_fill_volume(datasets, volume3D):
 
     return volume3D
 
-volume3D = sort_and_fill_volume(datasets, volume3D)
-
-print("Volume min intensity:", volume3D.min(),"Volume max intensity:", volume3D.max())
